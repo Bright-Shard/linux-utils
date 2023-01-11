e1d71d8fe3a8fd1888e59e0753894f837ff9b7ec
BrightShard <brightshard@brightshard.dev>
2023-01-10 19:48:01 -0500

    Fix symlinks

--- install.sh
+++ install.sh
@@ -1,17 +1,20 @@
 #!/bin/zsh
+
+export wd=$(pwd)
+
 echo "Install Rust? (Y/n)"
 read install_rust
 if [ $install_rust = 'Y' ] || [ $install_rust = 'y' ]
 then
-	curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain nightly --profile complete &>/dev/null </dev/null
+	$wd/PATH/stfu curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain nightly --profile complete
 	echo "Rust installed."
 fi
 
 echo "Setting up Zsh..."
-ln -sf .zshrc ~/.zshrc
+ln -sf $wd/.zshrc ~/.zshrc
 echo "Done."
 echo "Linking PATH folder..."
-ln -sf PATH ~/PATH
+ln -sf $wd/PATH ~/PATH
 echo "Done."
 
 echo "Install completed!"
