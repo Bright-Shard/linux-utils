570b352bf45bef213d8ea3dd438d6e847b8f073a
BrightShard <brightshard@brightshard.dev>
2023-01-10 19:44:44 -0500

    Fixed install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
 #!/bin/zsh
 echo "Install Rust? (Y/n)"
 read install_rust
-if [ $install_rust = 'Y' || $install_rust = 'y' ]
+if [ $install_rust = 'Y' ] || [ $install_rust = 'y' ]
 then
 	curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain nightly --profile complete &>/dev/null </dev/null
 	echo "Rust installed."
 fi
 
+echo "Setting up Zsh..."
 ln -sf .zshrc ~/.zshrc
+echo "Done."
+echo "Linking PATH folder..."
 ln -sf PATH ~/PATH
+echo "Done."
+
+echo "Install completed!"
