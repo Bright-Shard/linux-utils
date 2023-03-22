010cb3c811cfe5c0fada5eb74c0716d110b36c8a
BrightShard <brightshard@brightshard.dev>
2023-03-21 22:02:32 -0400

    Update installer to support Arch & Debian; Fix Rust installer; Install cargo-binstall and Starship

--- install.sh
+++ install.sh
@@ -1,20 +1,66 @@
-#!/bin/zsh
+#!/bin/bash
+clear
 
-export wd=$(pwd)
+# Confirmation message
 
-echo "Install Rust? (Y/n)"
-read install_rust
-if [ $install_rust = 'Y' ] || [ $install_rust = 'y' ]
+echo "This program will install:"
+echo "- Rust and Cargo (via Rustup)"
+echo "- Starship Prompt"
+echo "- Zsh"
+echo "- The .zshrc in this repo"
+echo "- The helpful scripts in ./PATH"
+echo ""
+echo "Proceed? (Y/n)"
+
+read PROCEED
+if [ $PROCEED != 'Y' ] && [ $PROCEED != 'y' ]
 then
-	$wd/PATH/stfu curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain nightly --profile complete
-	echo "Rust installed."
+	exit
 fi
 
+
+
+# Environment stuffs
+
+if [ -x "$(command -v apt)" ]
+then
+	INSTALL="apt install -y "
+elif [ -x "$(command -v pacman)" ]
+then
+	INSTALL="pacman -S --noconfirm "
+else
+	echo "ERROR: Failed to detect a package manager"
+	echo "Note: Currently, this script only supports APT and Pacman. Use a different package manager? Make a PR!"
+	exit
+fi
+STFU=$(pwd)/PATH/stfu
+
+
+# Actual installer
+
+echo ""
+echo "=== STARTING INSTALL SCRIPT ==="
+echo ""
+
+echo "Installing Zsh..."
+sudo $STFU $INSTALL zsh
+echo "Done."
+echo "Installing Rust..."
+$STFU curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain nightly --profile complete -y
+sudo $STFU $INSTALL pkgconf
+sudo $STFU $INSTALL cmake
+$STFU cargo install cargo-binstall
+echo "Done."
+echo "Installing Starship..."
+$STFU cargo binstall starship
+echo "Done."
 echo "Setting up Zsh..."
-ln -sf $wd/.zshrc ~/.zshrc
+ln -sf $(pwd)/.zshrc ~/.zshrc
 echo "Done."
 echo "Linking PATH folder..."
-ln -sf $wd/PATH ~/PATH
+ln -sf $(pwd)/PATH ~/PATH
 echo "Done."
 
-echo "Install completed!"
+echo ""
+echo "=== FINISHED INSTALL SCRIPT ==="
+echo ""
